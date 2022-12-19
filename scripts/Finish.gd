31e01bbd3cf37e21f5e03f941071a746278d4ed2
Hiebeler <emanuel.hiebeler@gmail.com>
2022-12-19 21:29:17 +0100

    add finish

---
+++ scripts/Finish.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+onready var body = get_tree().get_root().find_node("steve", true, false)
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	if overlaps_body(body):
+		print("finish")
+		get_tree().change_scene("res://scenes/finishedLevel.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,6 +6,5 @@ func _on_Level1_pressed():
 func _on_Level2_pressed():
 	get_tree().change_scene("res://scenes/level2.tscn")
 
-
 func _on_Level3_pressed():
 	get_tree().change_scene("res://scenes/level3.tscn")
