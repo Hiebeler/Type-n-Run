0cb7c29c482fe0e03b3ca9c0d9b48c0fec1a96e7
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-12-23 22:55:19 +0100

    create level 3

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _on_Level1_pressed():
-	get_tree().change_scene("res://scenes/level1.tscn")
+	get_tree().change_scene("res://scenes/levels/level1.tscn")
 
 func _on_Level2_pressed():
-	get_tree().change_scene("res://scenes/level2.tscn")
+	get_tree().change_scene("res://scenes/levels/level2.tscn")
 
 func _on_Level3_pressed():
-	get_tree().change_scene("res://scenes/level3.tscn")
+	get_tree().change_scene("res://scenes/levels/level3.tscn")
