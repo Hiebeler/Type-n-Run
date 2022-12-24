31a068b7f0f054afbadd8eee933a4bc5f176ff35
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-12-24 16:17:44 +0100

    add exit command, refactor startLevel function

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,4 @@
 extends Control
 
-func _on_Level1_pressed():
-	get_tree().change_scene("res://scenes/levels/level1.tscn")
-
-func _on_Level2_pressed():
-	get_tree().change_scene("res://scenes/levels/level2.tscn")
-
-func _on_Level3_pressed():
-	get_tree().change_scene("res://scenes/levels/level3.tscn")
+func startLevel(level):
+	get_tree().change_scene("res://scenes/levels/level" + level + ".tscn")
