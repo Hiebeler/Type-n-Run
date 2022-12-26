377514fe44c7981054921e00022cbb31efe04033
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-12-26 10:54:53 +0100

    implement dashing

--- scripts/steve.gd
+++ scripts/steve.gd
@@ -10,13 +10,20 @@ const RIGHT = "right"
 const LEFT = "left"
 const JUMP = "jump"
 const CROUCH = "crouch"
+const DASH = "dash"
 const STOP = "stop"
 const SPRINT = "sprint"
 enum StatesWalking { IDLE = 1, RIGHT, SPRINTRIGHT, LEFT, SPRINTLEFT }
 var stateWalking = StatesWalking.IDLE
+enum Facings { RIGHT = 1, LEFT}
+var facing = Facings.RIGHT
 var JumpCounter = 0
 var crouch = false
 var crouchTimer
+
+var canDash = true
+var dashing = false
+var dashDirection = Vector2.ZERO
 
 func _ready():
 	SignalBus.connect("LineEntered", self, "lineEntered")
@@ -52,29 +59,43 @@ func spring():
 	JumpCounter += 1
 	if JumpCounter <= 2:
 		velocity.y = SPRINGFORCE
+
+func dash():
+	if canDash:
+		if facing == Facings.RIGHT:
+			dashDirection = Vector2(1,0)
+		if facing == Facings.LEFT:
+			dashDirection = Vector2(-1,0)
+		velocity = dashDirection.normalized() * 3000
+		canDash = false
+		dashing = true
+		yield(get_tree().create_timer(0.5), "timeout")
+		dashing = false
+		canDash = true
 	
 func move():
-	match stateWalking:
-		StatesWalking.IDLE:
-			$Sprite.play("idle")
-		StatesWalking.RIGHT:
-			velocity.x = SPEED
-			$Sprite.flip_h = false
-			$Sprite.play("walk")
-		StatesWalking.SPRINTRIGHT:
-			velocity.x = SPRINTSPEED
-			$Sprite.flip_h = false
-			$Sprite.play("walk")
-		StatesWalking.LEFT:
-			velocity.x = -SPEED
-			$Sprite.flip_h = true
-			$Sprite.play("walk")
-		StatesWalking.SPRINTLEFT:
-			velocity.x = -SPRINTSPEED
-			$Sprite.flip_h = true
-			$Sprite.play("walk")
-	if crouch:
-		$Sprite.play("crouch")
+	if not dashing:
+		match stateWalking:
+			StatesWalking.IDLE:
+				$Sprite.play("idle")
+			StatesWalking.RIGHT:
+				velocity.x = SPEED
+				$Sprite.flip_h = false
+				$Sprite.play("walk")
+			StatesWalking.SPRINTRIGHT:
+				velocity.x = SPRINTSPEED
+				$Sprite.flip_h = false
+				$Sprite.play("walk")
+			StatesWalking.LEFT:
+				velocity.x = -SPEED
+				$Sprite.flip_h = true
+				$Sprite.play("walk")
+			StatesWalking.SPRINTLEFT:
+				velocity.x = -SPRINTSPEED
+				$Sprite.flip_h = true
+				$Sprite.play("walk")
+		if crouch:
+			$Sprite.play("crouch")
 
 func stopCrouch():
 	$CollisionShape2D.scale = Vector2(1, 1)
@@ -92,16 +113,22 @@ func startCrouch():
 func lineEntered(input):
 	if input == RIGHT:
 		stateWalking = StatesWalking.RIGHT
+		facing = Facings.RIGHT
 	elif input == (SPRINT + " " + RIGHT):
 		stateWalking = StatesWalking.SPRINTRIGHT
+		facing = Facings.RIGHT
 	elif input == LEFT:
 		stateWalking = StatesWalking.LEFT
+		facing = Facings.LEFT
 	elif input == (SPRINT + " " + LEFT):
 		stateWalking = StatesWalking.SPRINTLEFT
+		facing = Facings.LEFT
 	elif input == JUMP:
 		jump()
 	elif input == CROUCH and not crouch:
 		startCrouch()
+	elif input == DASH:
+		dash()
 
 	elif input == STOP:
 		stateWalking = StatesWalking.IDLE
